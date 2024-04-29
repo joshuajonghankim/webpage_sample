1efde6afe4f25aaad2f0f99bc9335784db3398e8
joshuajonghankim <joshuajonghankim@gmail.com>
2024-04-29 19:17:51 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className="sticky top-0 snap-center snap-always min-h-screen min-w-full flex justify-center items-center bg-background-pattern bg-cover">
           <div className="text-center text-wrap m-10">
-            <h1 className="text-2xl font-i_Melody text-gray-500">
+            <h1 className="text-1xl font-i_Melody text-gray-500">
               결혼합니다<br></br><br></br>
               하나님의 사랑으로 만난 두 사람이<br></br>
               이제 새로운 가정을 이루는 아름다운 약속을 하려 합니다.<br></br>
@@ -33,14 +33,10 @@ export default function Home() {
               <br></br>
               이영우
               신신숙
-              의 장남 이성연
-              <br></br>
+              의 장남 <strong>이성연</strong><br></br>
               김규백
               김영정
-              의 차녀 김한은
-
-
-
+              의 차녀 <strong>김한은</strong>
             </h1>
           </div>
         </div>
