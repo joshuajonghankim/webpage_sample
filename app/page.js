c18f9e5ab31212ada812695f8fb669b1996276b7
joshuajonghankim <joshuajonghankim@gmail.com>
2024-04-29 22:15:31 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -78,11 +78,11 @@ export default function Home() {
         </div>
 
         <div className="relative overflow-y-auto snap-center snap-always min-h-screen min-w-full flex justify-center items-center bg-background-pattern bg-cover bg-center">
-          <div className="absolute top-10 text-center text-black">
+          <div className="absolute top-20 text-center text-black">
             <h1 className="text-3xl font-Hahmlet font-bold text-black">방명록</h1>
 
           </div>
-          <div className='absolute top-20 h-auto text-black'>
+          <div className='absolute top-30 text-black'>
             <script async src="https://talk.hyvor.com/embed/embed.js" type="module"></script>
             <hyvor-talk-comments website-id="11017" page-id=""></hyvor-talk-comments>
           </div>
