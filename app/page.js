433abc28b5eeb7b216315d22d7e592a6cc87cf85
joshuajonghankim <joshuajonghankim@gmail.com>
2024-04-29 21:44:38 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -78,13 +78,13 @@ export default function Home() {
         </div>
 
         <div className="relative snap-center snap-always min-h-screen min-w-full flex justify-center items-center bg-background-pattern bg-cover bg-center">
-          <div className="absolute top-0 text-center">
+          <div className="absolute top-10 text-center">
             <h1 className="text-3xl font-Hahmlet font-bold text-black">방명록</h1>
             <script async src="https://talk.hyvor.com/embed/embed.js" type="module"></script>
             <hyvor-talk-comments website-id="11017" page-id=""></hyvor-talk-comments>
           </div>
 
-          <footer className='absolute bottom-0 text-sm text-black'>
+          <footer className='absolute text-sm text-black'>
             <p>&copy; 2024 joshuajonghankim. All rights reserved.</p>
           </footer>
 
