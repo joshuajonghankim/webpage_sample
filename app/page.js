347bb49be401aa8a0fe2db7219161dc5ddad499d
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-13 01:31:49 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -216,7 +216,7 @@ export default function Home() {
             </div>
 
             <div className="text-sm mx-10 text-wrap">
-              <h1 className="text-custom-blue text-left">지하철</h1>
+              <h1 className="text-custom-blue text-left"><br></br>지하철</h1>
               1호선 광명역 지하철 운행 (영등포 - 광명역)<br></br>
               1호선 관악역 1번 출구 (마을버스1-1 운행)<br></br><br></br>
               <h1 className="text-custom-blue text-left">주차 (2시간 무료)</h1>
@@ -247,7 +247,7 @@ export default function Home() {
             </h1>
 
             <div className="w-screen mt-10 px-10 text-sm">
-              <div className="border p-3 rounded-lg shadow-md mb-4 bg-bgcolor-sky">
+              <div className="border p-3 rounded-lg shadow-md mb-4 bg-bgcolor-sky text-gray-700">
                 <button
                   onClick={() => setShowGroomAccounts(!showGroomAccounts)}
                   className="w-full text-center flex justify-between items-center"
@@ -279,7 +279,7 @@ export default function Home() {
                 )}
               </div>
 
-              <div className="border p-3 rounded-lg shadow-md bg-bgcolor-sky">
+              <div className="border p-3 rounded-lg shadow-md bg-bgcolor-sky text-gray-700">
                 <button
                   onClick={() => setShowBrideAccounts(!showBrideAccounts)}
                   className="w-full text-center flex justify-between items-center"
