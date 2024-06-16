37aa022087fcfee5b6a6fe0ea935f5ddef98a0dc
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-16 08:11:41 -0400

    Update LivereComments.js

--- app/LivereComments.js
+++ app/LivereComments.js
@@ -1,38 +1,25 @@
-// pages/comments.js
-import Script from 'next/script';
-import Head from 'next/head';
+// components/LivereComments.js
 
-const CommentsPage = () => {
+import { useEffect } from 'react';
+
+const LivereComments = () => {
+  useEffect(() => {
+    // Check if Livere script is already loaded
+    if (typeof LivereTower !== 'function') {
+      const script = document.createElement('script');
+      script.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+      script.async = true;
+
+      const parentNode = document.getElementsByTagName('script')[0];
+      parentNode.parentNode.insertBefore(script, parentNode);
+    }
+  }, []);
+
   return (
-    <div>
-      <Head>
-        <title>Comments Page</title>
-      </Head>
-      
-      <div id="lv-container" data-id="city" data-uid="MTAyMC81OTkxNy8zNjM4MA=="></div>
+    <div id="lv-container" data-id="city" data-uid="MTAyMC81OTkxNy8zNjM4MA==">
       <noscript>라이브리 댓글 작성을 위해 JavaScript를 활성화 해주세요</noscript>
-      
-      <Script 
-        strategy="afterInteractive" // Ensures the script is loaded after the page is interactive
-        src="https://cdn-city.livere.com/js/embed.dist.js"
-      />
-
-      <Script id="livere-tower">
-        {`(function(d, s) {
-            var j, e = d.getElementsByTagName(s)[0];
-
-            if (typeof LivereTower === 'function') { return; }
-
-            j = d.createElement(s);
-            j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-            j.async = true;
-
-            e.parentNode.insertBefore(j, e);
-          })(document, 'script');`}
-      </Script>
     </div>
   );
 };
 
-export default CommentsPage;
-
+export default LivereComments;
