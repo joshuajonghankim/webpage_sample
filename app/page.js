34a862dfbc5d29938497c3af5adc984c2e69a333
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-16 05:54:55 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
 
   useEffect(() => {
     // 페이지가 로드될 때 자동 재생 시도    
-    setIsPlaying(true);    
+    setIsPlaying(true);
   }, []);
 
   const toggleMusic = () => {
@@ -238,18 +238,16 @@ export default function Home() {
               </a>
             </div>
 
-            <div className="text-xs mx-10 text-wrap">
-              <h1 className="text-custom-blue text-left"><br></br>지하철</h1>
-              1호선 광명역 지하철 운행 (영등포 - 광명역)<br></br>
-              1호선 관악역 1번 출구 (마을버스1-1 운행)<br></br><br></br>
-              <h1 className="text-custom-blue text-left">주차 (2시간 무료)</h1>
-              광명 센트럴자이 상가 주차장 B3층<br></br>
-              광명 파크자이 오피스텔 주차장 B2층<br></br><br></br>
-              <h1 className="text-custom-blue text-left text-wrap text-sm">※ 연회장 입구 직원분께 말씀해주시면 무료주차 2시간 입력해드립니다.</h1>
-            </div>
-          </div>
-          <div className="absolute top-2/3 w-auto">
 
+          </div>
+          <div className="absolute top-2/3 min-w font-MapoGoldenPier text-xs mx-10 text-wrap">
+            <h1 className="text-custom-blue text-left"><br></br>지하철</h1>
+            1호선 광명역 지하철 운행 (영등포 - 광명역)<br></br>
+            1호선 관악역 1번 출구 (마을버스1-1 운행)<br></br><br></br>
+            <h1 className="text-custom-blue text-left">주차 (2시간 무료)</h1>
+            광명 센트럴자이 상가 주차장 B3층<br></br>
+            광명 파크자이 오피스텔 주차장 B2층<br></br><br></br>
+            <h1 className="text-custom-blue text-left text-balance text-sm">※ 연회장 입구 직원분께 말씀해주시면 무료주차 2시간 입력해드립니다.</h1>
           </div>
         </div>
 
