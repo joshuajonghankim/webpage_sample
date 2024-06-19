d560d5139d05a8a6e4de82afe073989a60f30ee7
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-19 07:54:52 -0400

    icons

--- app/page.js
+++ app/page.js
@@ -288,16 +288,23 @@ export default function Home() {
         </div>
 
         <div className="relative snap-center snap-always min-h-screen min-w-full flex justify-center items-center bg-white bg-cover bg-center">
-          <h1 className="absolute top-20 text-xl font-MapoGoldenPier text-custom-blue">
-            참석여부 알리기
-          </h1>
-          <a className="absolute top-32 text-sm font-MapoGoldenPier rounded-xl p-2 text-gray-700 border border-gray-400 shadow-md"
-            href="https://forms.gle/S2SnGE7jT2SuxhF68"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            알리기
-          </a>
+          <div className="absolute top-20 flex flex-col items-center">
+            <svg className="mb-3 stroke-custom-blue size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+            </svg>
+
+            <h1 className="mb-6 text-xl font-MapoGoldenPier text-custom-blue">
+              참석여부 알리기
+            </h1>
+            
+            <a className="text-sm font-MapoGoldenPier rounded-xl p-2 text-gray-700 border border-gray-400 shadow-md"
+              href="https://forms.gle/S2SnGE7jT2SuxhF68"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              알리기
+            </a>
+          </div>
 
           <div className="absolute top-1/3 font-MapoGoldenPier flex flex-col items-center">
             <div className="flex justify-center items-center mb-3">
