b290d286e18c4c15b7d4dfaa8dc1b92690214dcb
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-21 23:23:30 -0400

    Update LivereComments.js

--- app/LivereComments.js
+++ app/LivereComments.js
@@ -55,7 +55,7 @@ const LivereComments = () => {
 
   return (
     <div className=''>
-      <div className="mt-20 text-center">
+      <div className="mt-12 text-center">
         <h1 className="text-sm font-MapoGoldenPier text-custom-blue">G U E S T B O O K</h1>
         <h1 className="text-xl font-MapoGoldenPier text-custom-blue">방명록</h1>
       </div>
