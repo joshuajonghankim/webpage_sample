d995943b276a801f1929069a1170aeca61840e85
joshuajonghankim <joshuajonghankim@gmail.com>
2024-06-21 21:02:26 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -320,13 +320,13 @@ export default function Home() {
               <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
             </svg>
 
-            <h1 className="mt-3 text-xl font-MapoGoldenPier text-custom-blue">
+            <h1 className="mt-2 text-xl font-MapoGoldenPier text-custom-blue">
               참석여부 알리기
             </h1>
 
             <button
               onClick={() => window.open('https://forms.gle/S2SnGE7jT2SuxhF68', '_blank')}
-              className="mt-3 w-8/10 flex items-center justify-center rounded-lg bg-bgcolor-sky relative"
+              className="mt-2 w-8/10 flex items-center justify-center rounded-lg bg-bgcolor-sky relative"
             >
               <div className="p-1 font-MapoGoldenPier text-center text-gray-700">
                 RSVN
@@ -337,8 +337,8 @@ export default function Home() {
             </button>
           </div>
 
-          <div className="absolute top-1/3 w-full font-MapoGoldenPier flex flex-col items-center">
-            <div className="flex justify-center items-center mb-3">
+          <div className="absolute top-3/10 w-full font-MapoGoldenPier flex flex-col items-center">
+            <div className="flex justify-center items-center">
               <Image
                 src="/images/heart.png"
                 alt="heart image"
@@ -348,11 +348,11 @@ export default function Home() {
               />
             </div>
 
-            <h1 className="text-xl text-custom-blue">
+            <h1 className="mt-2 text-xl text-custom-blue">
               마음 전하실 곳
             </h1>
 
-            <div className="w-full mt-3 flex flex-col items-center text-xs">
+            <div className="w-full mt-2 flex flex-col items-center text-xs">
               <div className="w-8/10 mb-4 border border-bgcolor-sky rounded-lg flex flex-col items-center">
                 <button
                   onClick={() => setShowGroomAccounts(!showGroomAccounts)}
